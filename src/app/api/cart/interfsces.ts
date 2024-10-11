58f5261610f8c97bff0c3f87c20c156e458cdcd3
Lifanovaa25 <50072172+Lifanovaa25@users.noreply.github.com>
2024-10-12 01:20:12 +0300

    cart

---
+++ src/app/api/cart/interfsces.ts
@@ -0,0 +1,58 @@
+import type { IResponseError } from '../interfaces';
+
+export interface IPageCartsRequestParams {
+  PageId: number;
+  cartItems: object[];
+}
+
+export interface ICartRequestParams {
+  PageId: number;
+  cartItems: {
+    Id: number;
+    Count: number;
+  };
+}
+
+export interface ICartResponse200 {
+  Value: {
+    items: [];
+  };
+  IsSuccess: boolean;
+  IsFailure: boolean;
+  Error: {
+    Code: string;
+    Description: string;
+    Type: string;
+  };
+}
+
+export interface ICartsResponse400 {
+  Value: {
+
+  type: string;
+  title: string;
+  status: number;
+  detail: string;
+  instance: string;
+  additionalProp1: string;
+  additionalProp2: string;
+  additionalProp3: string;
+  }
+}
+
+export interface ICartsResponse500 {
+  Result: {};
+  Value: {
+    type: string;
+    title: string;
+    status: number;
+    detail: string;
+    instance: string;
+    additionalProp1: string;
+    additionalProp2: string;
+    additionalProp3: string;
+  };
+}
+
+export type TPageCartsResponse = ICartsResponse400 | ICartsResponse500;
+export type IError = IResponseError | ICartsResponse400 | ICartsResponse500;
