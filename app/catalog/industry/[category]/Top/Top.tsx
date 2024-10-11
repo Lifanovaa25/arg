2349e240d1d5e88070b315dc52cc4cd864c35286
Lifanovaa25 <50072172+Lifanovaa25@users.noreply.github.com>
2024-10-11 22:40:24 +0300

    industry

---
+++ app/catalog/industry/[category]/Top/Top.tsx
@@ -0,0 +1,19 @@
+import Title from '@/src/shared/ui/Title/Title';
+import styles from './Top.module.scss';
+
+
+interface TopProps {
+  title: string;
+}
+
+export const Top = (props: TopProps) => {
+  const { title } = props;
+  return (
+    <section className={styles.section}>
+      <div className="container">
+        <Title className={styles.title} size="h1" dangerouslySetInnerHTML={{ __html: title }} >
+        </Title>
+      </div>
+    </section>
+  );
+};
