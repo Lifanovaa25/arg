3e620936f9aaaef5bb4fed63fc5c1a9ecdf25ad6
Lifanovaa25 <50072172+Lifanovaa25@users.noreply.github.com>
2024-10-11 21:06:55 +0300

    feedback

---
+++ src/app/api/feedback/feedbackAPI.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+import { IError, IFeedBackParams, TFeedBackResponse } from "./interfaces";
+
+const sendFeedback = async ({ name, phone,email,Itn,file,message }: IFeedBackParams): Promise<TFeedBackResponse | IError> => {
+  try {
+    let url = `${process.env.NEXT_PUBLIC_API_URL}/SendFeedback`
+    // &Name=${name}&Phone=${phone}&Email=${email}&Itn=${Itn}&Files=${file}&Message=${message}`;
+
+   
+    const response = await axios.post(url,{name,phone,email,file,Itn,message});
+    const result = await response.data;
+
+    return result;
+  } catch (error: unknown) {
+    const err = error as IError;
+    console.error(err);
+    return err;
+  }
+};
+
+export  {
+  sendFeedback
+};
---
+++ src/app/api/feedback/interfaces.ts
@@ -0,0 +1,51 @@
+import type { IResponseError } from '../interfaces';
+
+export interface IFeedBackParams {
+  name: string;
+  phone: string;
+  email: string | undefined;
+  Itn: string | null;
+  file: string | null;
+  message: string | undefined;
+}
+export interface IFeedBackResponse200 {
+  IsSuccess: boolean;
+  IsFailure: boolean;
+  Error: {
+    Code: string;
+    Description: string;
+    Type: string;
+  };
+  Value: string;
+}
+
+export interface IFeedBackResponse400 {
+  type: string;
+  title: string;
+  status: number;
+  detail: string;
+  instance: string;
+  additionalProp1: string;
+  additionalProp2: string;
+  additionalProp3: string;
+}
+
+export interface IFeedBackResponse500 {
+  Result: unknown;
+  Value: {
+    type: string;
+    title: string;
+    status: number;
+    detail: string;
+    instance: string;
+    additionalProp1: string;
+    additionalProp2: string;
+    additionalProp3: string;
+  };
+}
+
+export type TFeedBackResponse =
+  | IFeedBackResponse200
+  | IFeedBackResponse400
+  | IFeedBackResponse500;
+export type IError = IResponseError | IFeedBackResponse400 | IFeedBackResponse500;
