1868b468aba63c0e6d58f2681ebfeac877dc24e8
Lifanovaa25 <50072172+Lifanovaa25@users.noreply.github.com>
2024-10-12 00:00:36 +0300

    validation

--- src/shared/lib/validation/formSchema.ts
+++ src/shared/lib/validation/formSchema.ts
@@ -1,13 +1,15 @@
 import { z } from 'zod';
-
+const phoneRegex = new RegExp(
+  /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
+);
 export const FormSchema = z.object({
   name: z.string().min(1, 'Please enter name.'),
-  phone: z.string().min(1, 'Please enter phone.'),
+  phone: z.string().min(9, 'Please enter phone.').regex(phoneRegex, 'Invalid Number!'),
   message: z.string().optional(),
   email: z
     .string()
     .optional()
-    .refine((value) => !value || z.string().email().safeParse(value).success, {
+    .refine((value) => value || z.string().email().safeParse(value).success, {
       message: 'Please enter a valid email address.',
     }),
   accept: z.boolean().refine((val) => val === true, {
