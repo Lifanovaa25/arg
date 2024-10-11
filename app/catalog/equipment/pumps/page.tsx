33df3d90d978ebd91dd049277539cff0deb1bf67
Lifanovaa25 <50072172+Lifanovaa25@users.noreply.github.com>
2024-10-11 15:08:25 +0300

    fix

--- app/catalog/equipment/pumps/page.tsx
+++ app/catalog/equipment/pumps/page.tsx
@@ -1,5 +1,5 @@
 import { NextPage, Metadata } from 'next';
-import { Pumps } from '@/src/page/(catalog-pages)/Pumps';
+import { Pumps } from '@/src/page/(catalog-pages)/pumps';
 
 export const metadata: Metadata = {
   title: 'Pumps',
