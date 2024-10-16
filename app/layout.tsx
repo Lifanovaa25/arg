a38af1b8017a96bc6db8753013978e8ca7f6e4b5
Lifanovaa25 <50072172+Lifanovaa25@users.noreply.github.com>
2024-10-16 22:18:07 +0300

    sp

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,13 +15,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
+        {/* <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
           integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
-        />
+        /> */}
        {/* <link rel="icon" href="/public/favicon.png" />  */}
        {/* <link rel="icon" href="/favicon.png" sizes="any" /> */}
        <link rel="icon" href="/favicon2.ico" sizes="any" />
